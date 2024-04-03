bef480a0c365e0a837df6102237c289d44195d33
mithun30052001 <lmithun01@gmail.com>
2024-04-03 18:17:30 +0530

    form component is written

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TextInputComponent } from '../text-input/text-input.component';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+
+export class FormComponent{
+
+ 
+  
+  
+
+}
