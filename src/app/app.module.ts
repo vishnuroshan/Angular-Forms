9882d1335c7b4b48c11d76c09ebd0a897a023434
mithun30052001 <lmithun01@gmail.com>
2024-04-03 18:07:09 +0530

    necessary imports are added in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { TextInputComponent } from './text-input/text-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TextInputComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
