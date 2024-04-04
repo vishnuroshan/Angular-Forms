983774de5def40b050ed976f927e57e017042cc1
mithun30052001 <lmithun01@gmail.com>
2024-04-04 17:52:35 +0530

    Dropdown standalone component

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,16 +1,50 @@
-import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
   imports: [CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss']
+  styleUrls: ['./dropdown.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DropdownComponent),
+      multi: true
+    }
+  ]
 })
-export class DropdownComponent {
-  @Input() control: FormControl = new FormControl();
-  @Input() label: string = '';
-  @Input() options: { label: string; value: string }[] = [];
+export class DropdownComponent implements ControlValueAccessor {
+  @Input() options: any[] = [];
+  @Input() label: string = 'Select';
+  selectedOptions: any[] = [];
+
+  constructor() { }
+
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  writeValue(value: any): void {
+    if (Array.isArray(value)) {
+      this.selectedOptions = value;
+    } else {
+      this.selectedOptions = [];
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  selectOptions(options: any[]): void {
+    this.selectedOptions = options;
+    this.onChange(options);
+    this.onTouched();
+  }
 }
