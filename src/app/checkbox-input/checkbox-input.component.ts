9938db895f8b3d81f4a58714c1f5f7e5a133ece1
mithun30052001 <lmithun01@gmail.com>
2024-04-04 12:26:07 +0530

    checkbox standalone component is done successfully

--- src/app/checkbox-input/checkbox-input.component.ts
+++ src/app/checkbox-input/checkbox-input.component.ts
@@ -1,52 +1,15 @@
-import { Component, DestroyRef, forwardRef, inject, Input, OnInit } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
-import { noop } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-checkbox-input',
+  selector: 'app-checkbox',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './checkbox-input.component.html',
-  styleUrls: ['./checkbox-input.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => CheckboxInputComponent),
-      multi: true,
-    },
-  ],
+  styleUrls: ['./checkbox-input.component.scss']
 })
-export class CheckboxInputComponent implements ControlValueAccessor, OnInit {
-  @Input() label!: string;
-  @Input() formControlName!: string;
-
-  readonly formControl: FormControl = new FormControl();
-
-  private readonly destroyRef: DestroyRef = inject(DestroyRef);
-
-  onChange: (value: boolean) => void = noop;
-  onTouched: () => void = noop;
-
-  ngOnInit(): void {
-    this.formControl.valueChanges
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((checked: boolean): void => this.onChange(checked));
-  }
-
-  registerOnChange(fn: () => void): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
-
-  writeValue(checked: boolean): void {
-    this.formControl.patchValue(checked, { emitEvent: false });
-  }
-
-  setDisabledState?(isDisabled: boolean): void {
-    isDisabled ? this.formControl.disable() : this.formControl.enable();
-  }
+export class CheckboxInputComponent {
+  @Input() control: FormControl = new FormControl();
+  @Input() label: string = '';
+  @Input() options: { label: string; value: string }[] = [];
 }
