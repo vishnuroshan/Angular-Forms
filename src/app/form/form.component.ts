c5bc667def2aecd13c3401cf676b6c3921600b03
mithun30052001 <lmithun01@gmail.com>
2024-04-15 16:31:04 +0530

    custom validations are imported from helper and used in respective fields

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../helpers/custom-validators';
 
 @Component({
   selector: 'app-form',
@@ -46,25 +47,14 @@ export class FormComponent implements OnInit{
 
   createForm(){
     this.form = this.fb.group({
-      userName: [{value: 'defaultname', disabled: false }, [Validators.required, Validators.minLength(8), Validators.maxLength(15)]],
-      password: [{value: 'R@ils', disabled: false }, [Validators.required, this.passwordValidator()]],
+      userName: [{value: 'defaultname', disabled: false }, [Validators.required, Validators.minLength(8), Validators.maxLength(15),CustomValidators.whitespaceValidator()]],
+      password: [{value: 'R@ils', disabled: false }, [Validators.required, CustomValidators.passwordValidator()]],
       email: [{value: 'default@gmail.com', disabled: true },[Validators.required, Validators.email]],
       number: [{value: null, disabled: false },[Validators.required, Validators.min(1)]],
       dropdownOption: [{value: null, disabled: false }, [Validators.required]],
       radioOption: [{value: null, disabled: false }, []],
       checkboxOption: [{value: ["angular"], disabled: false }, [Validators.required]],
     })
-  }
-
-  passwordValidator(): Validators {
-    return (control: FormControl) => {
-      const value: string = control.value;
-      const hasUppercase = /[A-Z]/.test(value);
-      const hasLowercase = /[a-z]/.test(value);
-      const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(value);
-      const valid = hasUppercase && hasLowercase && hasSpecial;
-      return valid ? null : { invalidPassword: true };
-    };
   }
 
   onSubmit(formData: any) {
