3105b478b813313fadf9d5de3acdc9d75a09bd19
mithun30052001 <lmithun01@gmail.com>
2024-04-16 16:06:46 +0530

    svg code for icons are added here

---
+++ src/app/icons/icons.ts
@@ -0,0 +1,35 @@
+export const ICONS = {
+    successinfo: 
+        `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_3_2891)">
+            <path d="M9.99996 18.3333C14.6023 18.3333 18.3333 14.6023 18.3333 9.99996C18.3333 5.39759 14.6023 1.66663 9.99996 1.66663C5.39759 1.66663 1.66663 5.39759 1.66663 9.99996C1.66663 14.6023 5.39759 18.3333 9.99996 18.3333Z" stroke="#2B3874" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10 13.3333V10" stroke="#05C16E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10 6.66663H10.0083" stroke="#05C16E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_3_2891">
+            <rect width="20" height="20" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+        `,
+    errorinfo: 
+        `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_3_3106)">
+            <path d="M9.99996 18.3333C14.6023 18.3333 18.3333 14.6024 18.3333 10C18.3333 5.39763 14.6023 1.66667 9.99996 1.66667C5.39759 1.66667 1.66663 5.39763 1.66663 10C1.66663 14.6024 5.39759 18.3333 9.99996 18.3333Z" stroke="#FA7066" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10 13.3333V10" stroke="#FA7066" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10 6.66666H10.0083" stroke="#FA7066" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_3_3106">
+            <rect width="20" height="20" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>`,
+    email: 
+        `<svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.66663 3.83331L8.47073 8.59618C9.0217 8.98187 9.29719 9.17471 9.59685 9.2494C9.86154 9.31538 10.1384 9.31538 10.4031 9.2494C10.7027 9.17471 10.9782 8.98187 11.5292 8.59618L18.3333 3.83331M5.66663 14.6666H14.3333C15.7334 14.6666 16.4335 14.6666 16.9683 14.3942C17.4387 14.1545 17.8211 13.772 18.0608 13.3016C18.3333 12.7668 18.3333 12.0668 18.3333 10.6666V5.33331C18.3333 3.93318 18.3333 3.23312 18.0608 2.69834C17.8211 2.22793 17.4387 1.84548 16.9683 1.6058C16.4335 1.33331 15.7334 1.33331 14.3333 1.33331H5.66663C4.26649 1.33331 3.56643 1.33331 3.03165 1.6058C2.56124 1.84548 2.17879 2.22793 1.93911 2.69834C1.66663 3.23312 1.66663 3.93318 1.66663 5.33331V10.6666C1.66663 12.0668 1.66663 12.7668 1.93911 13.3016C2.17879 13.772 2.56124 14.1545 3.03165 14.3942C3.56643 14.6666 4.26649 14.6666 5.66663 14.6666Z" stroke="#667085" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    `
+}
+
