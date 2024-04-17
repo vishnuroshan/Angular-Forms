7ab01bbe87e9b917ae096005bc80293db65c63ba
mithun30052001 <lmithun01@gmail.com>
2024-04-17 10:15:25 +0530

    mobile number component added to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { CheckboxComponent } from './checkbox/checkbox.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ValidationErrorsComponent } from './validation-errors/validation-errors.component';
 import { TextAreaComponent } from './text-area/text-area.component';
+import { MobileNumberComponent } from './mobile-number/mobile-number.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,10 +28,12 @@ import { TextAreaComponent } from './text-area/text-area.component';
     RadioGroupInputComponent,
     CheckboxComponent,
     TextAreaComponent,
+    MobileNumberComponent,
     ValidationErrorsComponent,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
