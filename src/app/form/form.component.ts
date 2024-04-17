3da0d735633ecc5609b67111c04249a53175bc10
Tushar <tushar@sedintecnologis.com>
2024-04-17 14:29:07 +0530

    Bootstrap implementation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -29,7 +29,11 @@ export class FormComponent implements OnInit{
   dropdownOptions = [
     {label:'2 Years+', value:'two'},
     {label:'5 Years+', value:'five'},
-    {label:'10 Years+', value:'ten'}
+    {label:'10 Years+', value:'ten'},
+    {label:'10 Years+', value:'ten'},
+    {label:'10 Years+', value:'ten'},
+    {label:'10 Years+', value:'ten'},
+
   ]
 
   radioOptions = [
@@ -42,9 +46,8 @@ export class FormComponent implements OnInit{
     {label:'React Js', value:'react'},
     {label:'vue Js', value:'vue'}
   ]
-  
-  experience = `As a web developer,I have 2 years of experience crafting responsive and dynamic web applications. 
-                Proficient in HTML, CSS, JavaScript, and frameworks like React Angular and Rails`
+
+  experience = `As a web developer,I have 2 years of experience crafting responsive and dynamic web applications. Proficient in HTML, CSS, JavaScript, and frameworks like React Angular and Rails`
 
   ngOnInit(): void {
     this.createForm();
@@ -58,7 +61,7 @@ export class FormComponent implements OnInit{
       mobileNumber: [{ countrycode: '+91', mobile: '' }, [Validators.required]],
       number: [{value: null, disabled: false },[Validators.required, Validators.min(1)]],
       dropdownOption: [{value: null, disabled: false }, [Validators.required]],
-      radioOption: [{value: null, disabled: false }, []],
+      radioOption: [{value: null, disabled: false }, [Validators.required]],
       checkboxOption: [{value: ["angular"], disabled: false }, [Validators.required]],
       workExperience: [{value: this.experience.replace(/[\r\n]+/g, ' '), disabled: false }, [Validators.required, Validators.minLength(50), Validators.maxLength(200)]],
     })
